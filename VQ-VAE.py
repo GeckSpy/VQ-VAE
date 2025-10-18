c5b16bd3756bc589d874a3da74f10080aa3bd343
GeckSpy <maceo.ottavy@gmail.com>
2025-10-18 22:48:08 +0200

    uptade utils

--- VQ-VAE.py
+++ VQ-VAE.py
@@ -17,18 +17,31 @@ class Solver:
         self.model = MNIST_paper()
 
         self.loss = nn.MSELoss()#.cuda()
-        self.data = load_data(args)
+        self.data, self.data_loader = load_data(args)
 
         self.optimizer = torch.optim.Adam(self.model.parameters(),
                                           lr=self.args.learning_rate,
                                           betas=(0.5,0.999))
-        
+    
+
     
     def train(self):
         for epoch in range(self.args.epoches):
-            reconstruction_loss = []
-            sgz_e_loss = []
-            z_sge_loss = []
+            reconstruction_losses = []
+            sgz_e_losses = []
+            z_sge_losses = []
+
+            for id, (image, label) in enumerate(self.data):
+
+                X = ???
+
+                X_recon, Z_enc, Z_dec, Z_enc_for_embd = self.model(X)
+
+                reconstruction_loss = self.loss(X_recon, X)
+                sgz_e_loss = self.loss(self.model.embeding.weight, Z_enc_for_embd.detach())
+                z_sge_loss = self.loss(Z_enc, Z_dec.detach())
+                total_loss = reconstruction_loss + sgz_e_loss + z_sge_loss
+
 
 
 
--- utils.py
+++ utils.py
@@ -1,12 +1,42 @@
+import os
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
 
 
 class Arguments:
-    def __init__(self, epoches, learning_rate):
+    def __init__(self, epoches, learning_rate, dataset_name, batch_size):
         self.learning_rate = learning_rate
         self.epoches = epoches
+        self.dataset_name = dataset_name
+        self.batch_size = batch_size
 
 
 
 
-def load_data(args):
-    pass
+def load_data(args:Arguments):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),
+    ])
+    root = "./Datasests/"
+
+    if args.dataset_name == "MNIST":
+        root += "MNIST"
+        if not os.path.exists(root):
+            print("path to directory dataset: '", root ,"' does not exist")
+            raise ValueError
+
+        return
+        data = MNIST(root=root,
+                     train=True,
+                     transform=transform,
+                     download=True)
+        loader = DataLoader(data,
+                            batch_size=args.batch_size,
+                            shuffle=True,
+                            num_workers=4,
+                            drop_last=False)
+        
+arg = Arguments(epoches=100, learning_rate=1e-3, dataset_name="MNIST", batch_size=100)
+load_data(arg)
